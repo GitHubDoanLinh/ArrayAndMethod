5830d0e7818e97ac3150a422f37d6567169ba99e
GitHubDoanLinh <68548484+GitHubDoanLinh@users.noreply.github.com>
2023-05-07 14:40:50 +0700

    tim GTLN trong mang

--- src/FindMax.java
+++ src/FindMax.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class FindMax {
+import java.util.Scanner;
+
+public class FindMax {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("nhap vao kich thuoc mang : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("nhap vap gia tri cac phan tu ");
+        for(int k=0;k<arr.length;k++){
+            System.out.print("arr["+k+"] = ");
+            arr[k] = sc.nextInt();
+        }
+        System.out.println("mang da nhap la : ");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + "\t");
+        }
+        int max = arr[0];
+        int index=1;
+        for(int j=0;j<arr.length;j++){
+            if(arr[j]>max){
+                max = arr[j];
+                index=j;
+            }
+        }
+        System.out.println("\nmax = "+ max +" tai vi tri "+index);
+    }
 }
