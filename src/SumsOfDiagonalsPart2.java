4027f222d4c3800c611b1dcd72c0a60ee5360cec
GitHubDoanLinh <68548484+GitHubDoanLinh@users.noreply.github.com>
2023-05-09 09:02:50 +0700

    nhap vao ma tran va tinh tong duong cheo chinh

--- src/SumsOfDiagonalsPart2.java
+++ src/SumsOfDiagonalsPart2.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class SumsOfDiagonalsPart2 {
+import java.util.Scanner;
+
+public class SumsOfDiagonalsPart2 {
+    public static void main(String[] args) {
+
+        System.out.println("Nhap vao kich thuoc ma tran: ");
+        int n = new Scanner(System.in).nextInt();
+        int [][] matrix = new int[n][n];
+        System.out.println("Nhap vao gia tri cho ma tran");
+        for(int i=0;i<matrix.length;i++){
+            for (int j=0;j<matrix[i].length;j++){
+                System.out.print("matrix["+i+"]["+j+"] = ");
+                matrix[i][j] = new Scanner(System.in).nextInt();
+            }
+        }
+        int sum=0;
+        for(int k=0;k<n;k++){
+            for (int l=0;l<n;l++){
+                if(k==l){
+                    sum+=matrix[k][l];
+                }
+            }
+        }
+        System.out.println("tong duong cheo chinh = "+sum);
+
+    }
 }
