d27b23c94b59bf5ca08257720a95a9e0a107f439
GitHubDoanLinh <68548484+GitHubDoanLinh@users.noreply.github.com>
2023-05-09 08:58:39 +0700

    tinh tong duong cheo chinh cua ma tran

--- src/SumsOfDiagonals.java
+++ src/SumsOfDiagonals.java
@@ -8,25 +8,12 @@ public class SumsOfDiagonals {
 
     public static void main(String[] args) {
 
-//        System.out.println("NHAP VAO KICH THUOC HANG");
-//        int m = new Scanner(System.in).nextInt();
-//        System.out.println("NHAP VAO KICH THUOC COT");
-//        int n = new Scanner(System.in).nextInt();
-//        int[][] twoDArr = new int[m][n];
-//        System.out.println("Nhap vao gia tri cho " + twoDArr.length + " hàng và " +
-//                twoDArr[0].length + " cột: ");
-//        for (int row = 0; row < twoDArr.length; row++) {
-//            for (int column = 0; column < twoDArr[row].length; column++) {
-//                System.out.print("twoDArr[" + row + "]" + "[" + column + "] = ");
-//                twoDArr[row][column] = new Scanner(System.in).nextInt();
-
-
         int[][] a = {{1, 2, 3, 4},
                      {5, 6, 7, 8},
                      {1, 2, 3, 4},
                      {5, 6, 7, 8}};
 
-        printDiagonalSums(a, 4);
+        printDiagonalSums(a,  4);
     }
 
 
@@ -34,24 +21,20 @@ public class SumsOfDiagonals {
         int principal = 0, secondary = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-
-                // Condition for principal
-                // diagonal
+            // tong duong cheo phu
                 if (i == j)
                     principal += mat[i][j];
 
-                // Condition for secondary
-                // diagonal
-                if ((i + j) == (n - 1))
-                    secondary += mat[i][j];
+//                // tong duong cheo phu
+//                if ((i + j) == (n - 1))
+//                    secondary += mat[i][j];
             }
         }
 
-        System.out.println("Principal Diagonal:"
+        System.out.print("Tổng của đường chéo chính: "
                 + principal);
-
-        System.out.println("Secondary Diagonal:"
-                + secondary);
+//        System.out.println("Secondary Diagonal:"
+//                + secondary);
     }
 
 }
---
+++ src/SumsOfDiagonalsPart2.java
@@ -0,0 +1,2 @@
+package PACKAGE_NAME;public class SumsOfDiagonalsPart2 {
+}
