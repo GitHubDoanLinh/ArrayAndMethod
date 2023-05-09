fd5fde264ffa444abc57d332d6854d3695162a08
GitHubDoanLinh <68548484+GitHubDoanLinh@users.noreply.github.com>
2023-05-09 08:14:43 +0700

    chen phan tu vao mang

--- src/AddElement.java
+++ src/AddElement.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class AddElement {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AddElement {
+    public static void main(String[] args) {
+        int n = 10;
+        int arr[] = { 1, 2, 3, 4, 6, 7, 8, 0, 0 };
+
+        System.out.println("Mảng ban đầu: \n"
+                + Arrays.toString(arr));
+
+        System.out.print("Nhập vào số cần chèn: ");
+        int x = new Scanner(System.in).nextInt();
+        System.out.print("Nhập vào vị tri cần chèn: ");
+        int index = new Scanner(System.in).nextInt();
+        if(index <=-1 || index >= arr.length-1){
+            System.out.println("không có vị trí này");
+        }
+        else {
+            int tmp =0;
+            int tmp2=0;
+            boolean check=false;
+            for(int i=0;i<arr.length-1;i++){
+                if(i==index){
+                    tmp=arr[i];
+                    arr[i] = x;
+                    check=true;
+                    continue;
+                }
+                if(check){
+                    tmp2=arr[i];
+                    arr[i]=tmp;
+                    tmp=tmp2;
+
+                }
+
+            }
+            System.out.println("Mảng sau khi chèn: \n"
+                    + Arrays.toString(arr)+" tmp "+tmp);
+        }
+    }
 }
